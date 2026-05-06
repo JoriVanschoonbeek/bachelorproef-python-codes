6dd880f21867eed44d6f7c3b9c5b69644eef4078
JoriVanschoonbeek <p4wkjhpq6j@privaterelay.appleid.com>
2026-05-06 13:33:10 +0200

    Update 4_Fisher_kpp_impliciet_met_v.py

--- 4_Fisher_kpp_impliciet_met_v.py
+++ 4_Fisher_kpp_impliciet_met_v.py
@@ -3,12 +3,9 @@ import matplotlib.pyplot as plt
 
 def Fisher_kpp_snelheid_impliciet(D,r,v,L,N,T,t):
     dx = L/(N-1)        #de lengte van een discretisatiestap
-    dt = T/t      #t is het aantal tijdstappen
+    dt = T/t            #t is het aantal tijdstappen
     x = np.linspace(0,L,N)
-    #u = 0.5 * (1 - np.tanh(50 * (x - L/2)))
-    # u = np.exp(-10*((x-5)**2))
     u = 1/(1+np.exp(x-10))
-    # u = 1.05*np.ones(N)
 
     diffusie = (dt * D) / dx**2
     advectie = (dt * v) / dx
@@ -34,6 +31,8 @@ def Fisher_kpp_snelheid_impliciet(D,r,v,L,N,T,t):
         plt.title(f"Fisher-KPP impliciet, tijdstap = {k}")
         plt.draw()
         plt.pause(0.00001)
+
+        #afbeelding opslaan
         if k == 0:
             plt.savefig("impliciet_met_advectie_t=0.png")
         if k == 83:
@@ -42,4 +41,5 @@ def Fisher_kpp_snelheid_impliciet(D,r,v,L,N,T,t):
             plt.savefig("impliciet_met_advectie_t=120.png")
         if k == 249:
             plt.savefig("impliciet_met_advectie_t=249.png")
-print(Fisher_kpp_snelheid_impliciet(1, 1, 0.20, 20, 50, 10, 250))
+            
+#print(Fisher_kpp_snelheid_impliciet(1, 1, 0.20, 20, 50, 10, 250))
